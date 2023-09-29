44142c77f9bb5e59a0823922091028bf48e320fd
Francesco Passanante <francescopassanante@gmail.com>
2023-09-29 17:23:37 +0200

    fix: adding template to email

--- src/Controllers/BootstrapperController.php
+++ src/Controllers/BootstrapperController.php
@@ -7,9 +7,15 @@ use Mail;
 
 class BootstrapperController extends Controller
 {
-    public function sendEmail($subject, $data, $emailFrom, $emailTo)
+    public function sendEmail($subject, $emailFormatData, $emailTemplate, $emailFrom, $emailTo)
     {
-        Mail::send('emails.form-data', ['data' => $data], function ($m) use ($emailFrom, $emailTo, $subject) {
+        $body = formatDataForEmail($emailFormatData, $emailTemplate);
+
+        $data = [
+            'body' => $body
+        ];
+
+        Mail::send('emails.form-data', ['data' => $data], function ($m) use ($emailFrom, $emailTo, $subject){
             $m->from($emailFrom, env('MAIL_FROM_NAME'));
             $m->to($emailTo)->subject(env('APP_NAME').' '.$subject);
         });
