d6ffebb26dfdf54766fedb76a6a587cfbb7cb7ba
Giorgio <info@giorgiodominoni.it>
2024-02-12 18:33:39 +0100

    Fix subscription data table

--- src/views/settings.blade.php
+++ src/views/settings.blade.php
@@ -280,14 +280,21 @@
                 "autoWidth": false,
                 "responsive": false,
                 ajax: {
-                    url: "https://manager-fieroo.belicedigital.com/api/stripe/subscriptions",
+                    url: "http://127.0.0.1:8000/api/stripe/subscriptions",
                     type: 'POST',
                     headers: {
                         'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
                     },
-                    data: JSON.stringify({
-                        "email": $('input[name="auth-email"]').val()
-                    }),
+                    // data: JSON.stringify({
+                    //     "email": $('input[name="auth-email"]').val()
+                    // }),
+                    data: function(d) {
+                        const formData = new FormData();
+                        formData.append('email', $('input[name="auth-email"]').val());
+                        return formData;
+                    },
+                    contetType: false,
+                    processData: false,
                     dataSrc: 'subscriptions',
                 },
                 drawCallback: function() {
