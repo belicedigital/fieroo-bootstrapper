b03a5c228428794c72f986b398fc0dfc54dff4cc
Francesco Passanante <francescopassanante@gmail.com>
2024-07-15 22:05:16 +0200

    fix

--- src/Controllers/BootstrapperController.php
+++ src/Controllers/BootstrapperController.php
@@ -10,22 +10,25 @@ class BootstrapperController extends Controller
     public function sendEmail($subject, $emailFormatData, $emailTemplate, $emailFrom, $emailTo, $pdfContent = null, $pdfFileName= null)
     {
         $body = formatDataForEmail($emailFormatData, $emailTemplate);
-        if(!is_string($body)) {
-            dd('non è una stringa');
-        }
+
         $data = [
             'body' => $body
         ];
 
-        Mail::send('bootstrapper::emails.form-data', ['data' => $data], function ($m) use ($emailFrom, $emailTo, $subject, $pdfContent, $pdfFileName){
-            $m->from($emailFrom, env('MAIL_FROM_NAME'));
-            $m->to($emailTo)->subject(env('APP_NAME').' '.$subject);
-            // Attach PDF
-            if ($pdfContent && $pdfFileName) {
-                $m->attachData($pdfContent, $pdfFileName, [
-                    'mime' => 'application/pdf',
-                ]);
-            }
-        });
+        try {
+            Mail::send('bootstrapper::emails.form-data', ['data' => $data], function ($m) use ($emailFrom, $emailTo, $subject, $pdfContent, $pdfFileName){
+                $m->from($emailFrom, env('MAIL_FROM_NAME'));
+                $m->to($emailTo)->subject(env('APP_NAME').' '.$subject);
+                // Attach PDF
+                if ($pdfContent && $pdfFileName) {
+                    $m->attachData($pdfContent, $pdfFileName, [
+                        'mime' => 'application/pdf',
+                    ]);
+                }
+            });
+        } catch(\Exception $e) {
+            throw $e;
+        }
+        
     }
 }
